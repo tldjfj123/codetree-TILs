65f0f6e90ae37755fe1d57439f613574143a41fd
SeongJun Ahn <dksckd0804@naver.com>
2024-05-24 22:21:02 +0900

    Title: 변수 선언하기, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240524/변수 선언하기/declaring-variables.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        char c = 'C';
+
+        System.out.println(a);
+        System.out.println(c);
+    }
+}
