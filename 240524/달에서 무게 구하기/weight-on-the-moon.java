7253cbf697307a2a747c6a492f1c95165b90e1d3
SeongJun Ahn <dksckd0804@naver.com>
2024-05-24 22:56:02 +0900

    Title: 달에서 무게 구하기, Time: 89ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240524/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 13;
+        double b = 0.165;
+
+        double res = a * b;
+
+        System.out.print(a + " ");
+        System.out.print("*" + " ");
+        System.out.printf("%.6f", b);
+        System.out.print(" ");
+        System.out.print("=" + " ");
+        System.out.printf("%.6f", res);
+
+        
+
+
+    }
+}
