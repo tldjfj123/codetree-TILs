86870f2d3bbfcaf7206444dd0a8e24c312c236d9
SeongJun Ahn <dksckd0804@naver.com>
2024-05-25 21:05:20 +0900

    Title: 삼항연산자 2, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240525/삼항연산자 2/ternary-operator-2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        String res = a == 1 ? "t" : "f";
+
+        System.out.println(res);
+    }
+}
