681b533fc63360c75c018eb7acd280bf1156c9d9
SeongJun Ahn <dksckd0804@naver.com>
2024-05-25 15:57:05 +0900

    Title: 세 실수의 반올림, Time: 165ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240525/세 실수의 반올림/rounding-of-three-actual-numbers.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
+
+        System.out.printf("%.3f", a);
+        System.out.print("\n");
+        System.out.printf("%.3f", b);
+        System.out.print("\n");
+        System.out.printf("%.3f", c);
+    }
+}
