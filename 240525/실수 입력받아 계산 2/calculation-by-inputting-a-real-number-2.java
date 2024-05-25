dd12011898b2e6a055bc57122ab6c55642933956
SeongJun Ahn <dksckd0804@naver.com>
2024-05-25 15:30:11 +0900

    Title: 실수 입력받아 계산 2, Time: 143ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240525/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextDouble();
+        
+        double res = a + 1.5;
+
+        System.out.printf("%.2f", res);
+
+    }
+}
