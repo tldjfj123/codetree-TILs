23a51019bad90120ad61d7efee2b9e747c78b825
SeongJun Ahn <dksckd0804@naver.com>
2024-05-25 21:59:41 +0900

    Title: 최대 2번의 연산, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240525/최대 2번의 연산/up-to-2-calculations.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        if (a % 2 == 0) {
+            a /= 2;
+        }
+
+        if (a %2 == 1) {
+            a += 1;
+            a /= 2;
+        }
+
+        System.out.println(a);
+    }
+}
