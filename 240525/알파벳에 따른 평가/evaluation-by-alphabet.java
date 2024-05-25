1ec9bc30f4baa9a4c68a9d1b53b67e87ac81a2ed
SeongJun Ahn <dksckd0804@naver.com>
2024-05-25 21:52:38 +0900

    Title: 알파벳에 따른 평가, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240525/알파벳에 따른 평가/evaluation-by-alphabet.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+
+        if (s.equals("S")) {
+            System.out.println("Superior");
+        } else if (s.equals("A")) {
+            System.out.println("Excellent");
+        } else if (s.equals("B")) {
+            System.out.println("Good");
+        } else if (s.equals("C")) {
+            System.out.println("Usually");
+        } else if (s.equals("D")) {
+            System.out.println("Effort");
+        } else {
+            System.out.println("Failure");
+        }
+    }
+}
