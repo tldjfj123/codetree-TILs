65b6dd659a8826e49d3f9ffcfa00e04796738b6a
SeongJun Ahn <dksckd0804@naver.com>
2024-05-25 15:10:53 +0900

    Title: 변수 값 교체하기 6, Time: 82ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240525/변수 값 교체하기 6/replacing-variable-values-6.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 5;
+
+        int tmp = a;
+        a = b;
+        b = tmp;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
