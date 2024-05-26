202f52acb2e1554937f504b02eb4d6c04a520f03
SeongJun Ahn <dksckd0804@naver.com>
2024-05-26 15:56:49 +0900

    Title: 배수의 개수 2, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240526/배수의 개수 2/number-of-multipliers-2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cnt = 0;
+
+        for (int i = 0; i < 10; i++) {
+            int n = sc.nextInt();
+
+            if (n % 2 == 1) {
+                cnt++;
+            }
+        }
+
+
+        System.out.print(cnt);
+    }
+}
