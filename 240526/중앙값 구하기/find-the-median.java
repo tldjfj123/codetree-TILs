8eca6d9e43cf0f6204e951b9a9840a8c1906177d
SeongJun Ahn <dksckd0804@naver.com>
2024-05-26 11:46:19 +0900

    Title: 중앙값 구하기, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 240526/중앙값 구하기/find-the-median.java
+++ 240526/중앙값 구하기/find-the-median.java
@@ -4,7 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String[] arr = sc.nextLine().split(" ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int[] arr = {a, b, c};
+
+        Arrays.sort(arr);
 
         System.out.println(arr[1]);
     }
