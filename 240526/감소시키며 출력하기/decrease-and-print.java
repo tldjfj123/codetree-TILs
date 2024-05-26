eec96f7c6f269212506a6e304c33d69bd24fb2fa
SeongJun Ahn <dksckd0804@naver.com>
2024-05-26 12:04:24 +0900

    Title: 감소시키며 출력하기, Time: 84ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240526/감소시키며 출력하기/decrease-and-print.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 17; i > 4; i--) {
+            System.out.print(i + " ");
+        }
+    }
+}
