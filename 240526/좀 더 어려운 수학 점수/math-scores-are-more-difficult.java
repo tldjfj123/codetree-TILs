ea50faa72b6733c00bf2ba8951f5eeb2c859eced
SeongJun Ahn <dksckd0804@naver.com>
2024-05-26 10:28:10 +0900

    Title: 좀 더 어려운 수학 점수, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240526/좀 더 어려운 수학 점수/math-scores-are-more-difficult.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int aM = sc.nextInt();
+        int aE = sc.nextInt();
+        int bM = sc.nextInt();
+        int bE = sc.nextInt();
+
+        if (aM > bM) {
+            System.out.println("A");
+        } else {
+            System.out.println("B");
+        }
+
+        if (aM == bM) {
+            if (aE > bE) {
+                System.out.println("A");
+            } else {
+                System.out.println("B");
+            }
+        }
+    }
+}
