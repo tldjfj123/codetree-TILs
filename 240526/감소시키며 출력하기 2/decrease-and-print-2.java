d3e9ad3c80cfd0955f99cc2aa5f3c9e02180cadd
SeongJun Ahn <dksckd0804@naver.com>
2024-05-26 13:36:39 +0900

    Title: 감소시키며 출력하기 2, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240526/감소시키며 출력하기 2/decrease-and-print-2.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 26;
+
+        while (n > 9) {
+            System.out.print(n + " ");
+            n--;
+        }
+    }
+}
