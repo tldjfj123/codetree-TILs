faa66608ec832b550ed487e325bbea8c8ee737a6
SeongJun Ahn <dksckd0804@naver.com>
2024-05-27 20:13:02 +0900

    Title: 특정 숫자 맞추기, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240527/특정 숫자 맞추기/catching-specific-number.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            int n = sc.nextInt();
+
+            if (n > 25) {
+                System.out.println("Lower");
+            } else if (n < 25) {
+                System.out.println("Higher");
+            } else {
+                System.out.print("Good");
+                break;
+            }
+        }
+    }
+}
