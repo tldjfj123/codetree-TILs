67a5c9709348a3ebdc048f6ff56cd2c8066f03b7
SeongJun Ahn <dksckd0804@naver.com>
2024-05-27 21:35:11 +0900

    Title: 조건에 따른 연산, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240527/조건에 따른 연산/operatino-by-rule.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int cnt = 0;
+
+        while(n <= 1000) {
+            if(n % 2 == 0) {
+                n = 3 * n + 1;
+            } else {
+                n = 2 * n + 2;
+            }
+            cnt++;
+        }
+
+        System.out.print(cnt);
+
+    }
+}
