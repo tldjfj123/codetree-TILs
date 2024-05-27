514e860642ceabd5f1f4370a7de9faabd4974a46
SeongJun Ahn <dksckd0804@naver.com>
2024-05-27 21:33:04 +0900

    Title: 2의 거듭제곱수, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240527/2의 거듭제곱수/pow-of-2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int cnt = 0;
+
+        while (n != 1) {
+            n /= 2;
+            cnt++;
+        }
+
+        System.out.print(cnt);
+    }
+}
