8b15e02ecbe0f3e9d68298ea7bc120d1d9314760
SeongJun Ahn <dksckd0804@naver.com>
2024-05-28 22:03:07 +0900

    Title: 별 그리기, Time: 308ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240528/별 그리기/star-drawing.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - (i + 1); j++) {
+                System.out.print(" ");
+            }
+            
+            for (int k = 0; k < (2 * i) + 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 0; k < (2 * n) - (2 * i) - 3; k++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
