824430d6cc84eeee06372d5c6722efa418981693
SeongJun Ahn <dksckd0804@naver.com>
2024-05-28 14:21:14 +0900

    Title: ab 사이에 없는 c, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240528/ab 사이에 없는 c/c-between-a-and-b-2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        String res = "YES";
+
+        for (int i = a; i <= b; i++) {
+            if (i % c == 0) {
+                res = "NO";
+                break;
+            }
+        }
+
+        System.out.print(res);
+    }
+}
