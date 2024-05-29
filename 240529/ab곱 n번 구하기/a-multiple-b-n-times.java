c69381a9a593d6a4a5ccbef66676eae77cf53c39
SeongJun Ahn <dksckd0804@naver.com>
2024-05-29 22:05:18 +0900

    Title: ab곱 n번 구하기, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240529/ab곱 n번 구하기/a-multiple-b-n-times.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            int prod = 1;
+
+            for (int j = a; j <= b; j++) {
+                prod *= j;
+            }
+
+            System.out.println(prod);
+        }
+    }
+}
