bcd8f9f978a4e0e4d5f73fdcc7ae0a3cc88cd566
SeongJun Ahn <dksckd0804@naver.com>
2024-05-29 21:59:04 +0900

    Title: 알파벳 출력하기 2, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240529/알파벳 출력하기 2/print-alphabet-2.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        char c = 'A';
+
+        for (int i = 0; i < N; i++) {
+            //공백
+            for (int j = 0; j < i; j++) {
+                System.out.print("  ");
+            }
+
+            //문자
+            for (int k = 0; k < N - i; k++) {
+                System.out.print(c + " ");
+                c++;
+            }
+
+            System.out.println();
+        }
+    }
+}
