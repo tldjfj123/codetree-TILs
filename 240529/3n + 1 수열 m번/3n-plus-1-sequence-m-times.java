1e4bc0bf8a0add7e4141a5fb90fdd9eb22e37726
SeongJun Ahn <dksckd0804@naver.com>
2024-05-29 22:08:49 +0900

    Title: 3n + 1 수열 m번, Time: 171ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240529/3n + 1 수열 m번/3n-plus-1-sequence-m-times.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int m = sc.nextInt();
+
+        for (int i = 0; i < m; i++) {
+            int n = sc.nextInt();
+            int cnt = 0;
+
+            while (n != 1) {
+                if (n % 2 == 0) {
+                    n /= 2;
+                } else {
+                    n = 3 * n + 1;
+                }
+                cnt++;
+            }
+
+            System.out.println(cnt);
+        }
+    }
+}
