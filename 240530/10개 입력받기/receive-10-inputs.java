56417886173277acf5368e350264c8e82175560b
SeongJun Ahn <dksckd0804@naver.com>
2024-05-30 10:39:47 +0900

    Title: 10개 입력받기, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240530/10개 입력받기/receive-10-inputs.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[10];
+        int cnt = 0;
+
+        for (int i = 0; i < 10; i++) {
+            int n = sc.nextInt();
+
+            if (n == 0) {
+                break;
+            }
+            arr[i] = n;
+            cnt++;
+        }
+
+        int sum = 0;
+        for (int i = 0; i < cnt; i++) {
+            sum += arr[i];
+        }
+
+        double avg = sum / (double)cnt;
+
+        System.out.print(sum + " ");
+        System.out.printf("%.1f", avg);
+    }
+}
