9c5c706058ced62a4f51e9331336d9be84ee21b6
SeongJun Ahn <dksckd0804@naver.com>
2024-05-30 15:22:27 +0900

    Title: 특정 위치의 숫자, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240530/특정 위치의 숫자/numbers-in-specific-location.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+ 
+        for (int i = 0; i < 10; i++) {
+            int n = sc.nextInt();
+
+            arr[i] = n;
+        }
+
+        System.out.print(arr[2] + arr[4] + arr[9]);
+    }
+}
