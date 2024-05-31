098e985ed7b59855a058f77a585b07f4c291b835
SeongJun Ahn <dksckd0804@naver.com>
2024-05-31 13:39:10 +0900

    Title: 가장 큰 수 고르기, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240531/가장 큰 수 고르기/picking-biggest-number.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int max = 0;
+
+        for (int i = 0; i < 10; i++) {
+            int n = sc.nextInt();
+
+            if (n >= max) {
+                max = n;
+            }
+        }
+
+        System.out.println(max);
+
+    }
+}
