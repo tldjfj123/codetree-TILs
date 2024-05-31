3fbc627edfafb137a916ed95dc537a3bbabe10f6
SeongJun Ahn <dksckd0804@naver.com>
2024-05-31 14:51:06 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 137ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240531/가장 왼쪽에 있는 최댓값/leftmost-max-value.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            int n = sc.nextInt();
+            arr[i] = n;
+        }
+
+        while (arr.length > 1) {
+            int maxVal = -1;
+            
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] >= maxVal) {
+                    maxVal = arr[i];
+                }
+            }
+
+            int idx = 0;
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] == maxVal) {
+                    idx = i;
+                    break;
+                }
+            }
+            
+            System.out.print(idx+1 + " ");
+
+            arr = Arrays.copyOfRange(arr, 0, idx);
+        }
+
+        
+        
+    }
+}
