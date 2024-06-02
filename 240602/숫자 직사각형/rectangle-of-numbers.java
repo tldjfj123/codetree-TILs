ab7acd4c2d316ad819dae3ea713399f8f497b54b
SeongJun Ahn <dksckd0804@naver.com>
2024-06-02 13:56:53 +0900

    Title: 숫자 직사각형, Time: 205ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240602/숫자 직사각형/rectangle-of-numbers.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[][] arr = new int[n][m];
+
+        int v = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = v;
+                v++;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
