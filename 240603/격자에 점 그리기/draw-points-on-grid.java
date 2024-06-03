b21e4f29f6a15ba2002ce757207590f0dfdd61f4
SeongJun Ahn <dksckd0804@naver.com>
2024-06-03 17:11:54 +0900

    Title: 격자에 점 그리기, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240603/격자에 점 그리기/draw-points-on-grid.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[][] arr = new int[n][n];
+        int v = 1;
+
+        for (int i = 0; i < m; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            arr[x-1][y-1] = v;
+            v++;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
