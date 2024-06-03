4da954956cc4be385973c0251e6c0b9453acf887
SeongJun Ahn <dksckd0804@naver.com>
2024-06-03 18:03:35 +0900

    Title: 단어로 구분하기, Time: 116ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240603/단어로 구분하기/separate-words-with-words.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] arr = sc.nextLine().split(" ");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
