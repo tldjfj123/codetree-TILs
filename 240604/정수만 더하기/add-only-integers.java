44920995a4f7f90441674544c1d3c12d04119acb
SeongJun Ahn <dksckd0804@naver.com>
2024-06-04 15:12:00 +0900

    Title: 정수만 더하기, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

--- 240604/정수만 더하기/add-only-integers.java
+++ 240604/정수만 더하기/add-only-integers.java
@@ -9,7 +9,7 @@ public class Main {
 
         for (int i = 0; i < s.length(); i++) {
             if ('0' <= s.charAt(i) && s.charAt(i) <= '9') {
-                sum += Character.getNumericValue(s.charAt(i));
+                sum += s.charAt(i) - '0';
             }
         }
 
