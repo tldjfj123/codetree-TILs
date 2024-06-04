63f0b6f0574a05248e6f0dad21849a27955cc157
SeongJun Ahn <dksckd0804@naver.com>
2024-06-04 14:36:41 +0900

    Title: 아스키코드의 합과 차, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240604/아스키코드의 합과 차/sum-and-subtract-in-ASCII.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = (int)sc.next().charAt(0);
+        int b = (int)sc.next().charAt(0);
+
+        System.out.print((a+b) + " " + (int)(Math.abs(a-b)));
+    }
+}
