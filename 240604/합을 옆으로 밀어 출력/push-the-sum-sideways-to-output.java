bb993e51a681f395c0ca1ff49efad505c32d9cd8
SeongJun Ahn <dksckd0804@naver.com>
2024-06-04 15:53:34 +0900

    Title: 합을 옆으로 밀어 출력, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240604/합을 옆으로 밀어 출력/push-the-sum-sideways-to-output.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += sc.nextInt();
+        }
+
+        String s = Integer.toString(sum);
+
+        System.out.print(s.substring(1, s.length()) + s.substring(0, 1));
+    }
+}
