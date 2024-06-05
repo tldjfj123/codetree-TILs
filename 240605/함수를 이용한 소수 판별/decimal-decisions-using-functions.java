cd1e50ec4d37798cfff9a9f13b252bf57a766ac3
SeongJun Ahn <dksckd0804@naver.com>
2024-06-05 13:48:21 +0900

    Title: 함수를 이용한 소수 판별, Time: 163ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240605/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();   
+
+        int sum = 0;
+
+        for (int i = a; i <= b; i++) {
+            if (isPrime(i)) {
+                sum += i;
+            }
+        }
+
+        System.out.print(sum);
+    }
+
+    static boolean isPrime(int n) {
+        if (n == 1) {
+            return false;
+        }
+
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+
+
+}
