e3966240f24dfd1dc2b22013eeb53db37b565488
SeongJun Ahn <dksckd0804@naver.com>
2024-06-05 12:24:37 +0900

    Title: 함수를 이용해 직사각형 만들기, Time: 191ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240605/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        make(n, m);
+    }
+
+    public static void make(int n, int m) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print("1");
+            }
+            System.out.println();
+        }
+    }
+}
