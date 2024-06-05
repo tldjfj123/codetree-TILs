c9fb6bb7ca1142089bafd52ef06c085004ed9d9e
SeongJun Ahn <dksckd0804@naver.com>
2024-06-05 13:09:32 +0900

    Title: 정수의 최솟값, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240605/정수의 최솟값/minimum-value-of-an-integer.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        System.out.print(getMin(a, b, c));
+    }
+
+    static int getMin(int a, int b, int c) {
+        int min = Integer.MAX_VALUE;
+
+        if (min > a) {
+            min = a;
+        }
+
+        if (min > b) {
+            min = b;
+        }
+
+        if (min > c) {
+            min = c;
+        }
+
+        return min;
+    }
+}
