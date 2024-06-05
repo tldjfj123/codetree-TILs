1adcefdc61ef8e65be0fd7ff57e29670d16d723d
SeongJun Ahn <dksckd0804@naver.com>
2024-06-05 13:59:58 +0900

    Title: 함수를 이용한 윤년 판별, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240605/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.java
+++ 240605/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.java
@@ -10,7 +10,7 @@ public class Main {
     }
 
     static boolean check(int n) {
-        if (n % 4 == 0 || (n % 100 == 0 && n % 400 != 0)) {
+        if (n % 4 == 0 || !(n % 100 == 0 && n % 400 != 0)) {
             return true;
         }
         return false;
