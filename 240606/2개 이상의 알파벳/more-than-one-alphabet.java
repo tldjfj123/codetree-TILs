14164a97fda729e633fdf108c70eaa5e89870182
SeongJun Ahn <dksckd0804@naver.com>
2024-06-06 21:18:27 +0900

    Title: 2개 이상의 알파벳, Time: 116ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240606/2개 이상의 알파벳/more-than-one-alphabet.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+
+        System.out.print(check(s));
+    }
+
+    static String check(String s) {
+        if (s.length() == 1) {
+            return "No";
+        }
+
+        char[] arr = s.toCharArray();
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[0] != arr[i]) {
+                return "Yes";
+            }
+        }
+
+        return "NO"; 
+    }
+}
