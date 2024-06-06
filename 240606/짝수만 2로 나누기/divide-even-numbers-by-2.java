0539c5fe6321eb91e10adb5b1c18726638ab43c1
SeongJun Ahn <dksckd0804@naver.com>
2024-06-06 20:37:34 +0900

    Title: 짝수만 2로 나누기, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240606/짝수만 2로 나누기/divide-even-numbers-by-2.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        modify(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+
+    static void modify(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                arr[i] /= 2;
+            }
+        }
+    }
+}
