9754a455fb289daa069a101564d2ecd77ef0bf0a
SeongJun Ahn <dksckd0804@naver.com>
2024-06-08 20:22:21 +0900

    Title: 2개씩 그룹짓기, Time: 213ms, Memory: 12MB, Status: Wrong Answer - CodeTree

--- 240608/2개씩 그룹짓기/group-of-pairs.java
+++ 240608/2개씩 그룹짓기/group-of-pairs.java
@@ -1,21 +1,28 @@
 import java.util.*;
 
 public class Main {
+    static int res = 0;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
+
         List<Integer> arr = new ArrayList<Integer>();
 
         for (int i = 0; i < 2*n; i++) {
             arr.add(sc.nextInt());
         }
 
+        if (n == 1) {
+            for (Integer a : arr) {
+                res += a;
+            }    
+
+            System.out.print(res);    
+        } else {
         Collections.sort(arr);
 
         List<Integer> sub = arr.subList(2, n+1);
-
-        int res = 0;
 
         for (Integer s : sub) {
             res += s;
@@ -24,5 +31,6 @@ public class Main {
         res = Math.max(res + arr.get(0), res + arr.get(1));
 
         System.out.print(res);
+        }
     }
 }
