c21f580809b9dd4ccaaba7f1a53e4b9bf275cd9c
SeongJun Ahn <dksckd0804@naver.com>
2024-06-13 00:08:54 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 159ms, Memory: 42MB, Status: Wrong Answer - CodeTree

---
+++ 240613/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -0,0 +1,80 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[][] board = new int[2001][2001];
+        int[][] board2 = new int[2001][2001];
+
+        int x1 = sc.nextInt();
+        int y1 = sc.nextInt();
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();
+
+        int x3 = sc.nextInt();
+        int y3 = sc.nextInt();
+        int x4 = sc.nextInt();
+        int y4 = sc.nextInt();
+
+        int offset = 0;
+
+        if (x1 < 0) {
+            offset = Math.min(x1, offset);
+        }
+
+        if (y1 < 0) {
+            offset = Math.min(y1, offset);
+        }
+
+        if (x3 < 0) {
+            offset = Math.min(x3, offset);
+        }
+
+        if (y3 < 0) {
+            offset = Math.min(y3, offset);
+        }
+
+        offset *= -1;
+        
+        int cnt = 0;
+        for (int i = x1 + offset; i < x2 + offset; i++) {
+            for (int j = y1 + offset; j < y2 + offset; j++) {
+                board[i][j] = 1;
+                cnt++;
+            }
+        }
+
+        for (int i = x3 + offset; i < x4 + offset; i++) {
+            for (int j = y3 + offset; j < y4 + offset; j++) {
+                board2[i][j] = 1;
+            }
+        }
+
+        //세로 계산
+        int resY = Integer.MIN_VALUE;
+        for (int i = x1 + offset; i < x2 + offset; i++) {
+            int tmp = 0;
+            for (int j = y1 + offset; j < y2 + offset; j++) {
+                if (board[i][j] == 1 && board2[i][j] != 1) {
+                    tmp++;
+                } 
+            }
+            resY = Math.max(resY, tmp);
+        }
+
+        int resX = Integer.MIN_VALUE;
+        for (int i = y1 + offset; i < y2 + offset; i++) {
+            int tmp = 0;
+            for (int j = x1 + offset; j < x2 + offset; j++) {
+                if (board[j][i] == 1 && board2[j][i] != 1) {
+                    tmp++;
+                } 
+            }
+            resX = Math.max(resX, tmp);
+        }
+
+        System.out.print(resX * resY);
+
+    }
+}
