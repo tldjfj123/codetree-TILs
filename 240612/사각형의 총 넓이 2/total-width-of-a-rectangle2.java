7419ed31eb3bc435f066e832a4ecfda91b554cf7
SeongJun Ahn <dksckd0804@naver.com>
2024-06-12 16:11:01 +0900

    Title: 사각형의 총 넓이 2, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

--- 240612/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
+++ 240612/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
@@ -1,17 +1,29 @@
 import java.util.*;
 
+class Dot {
+    int x1; 
+    int y1;
+    int x2;
+    int y2;
+
+    public Dot(int x1, int y1, int x2, int y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();   
 
-        int offset = 0;
-
-        int[][] arr = new int[n][2];
+        Dot[] arr = new Dot[n];
         int[][] board = new int[200][200];
 
-
+        int offset = 0;
         for (int k = 0; k < n; k++) {
             int x1 = sc.nextInt();
             int y1 = sc.nextInt();
@@ -26,13 +38,18 @@ public class Main {
                 offset = Math.min(offset, y1);
             }
 
-            for (int i = x1 + offset; i < x2 + offset; i++) {
-                for (int j = y1 + offset; j < y2 + offset; j++) {
+            arr[k] = new Dot(x1, y1, x2, y2);
+        }
+        
+        offset *= -1;
+
+        for (Dot a : arr) {
+            for (int i = a.x1 + offset; i < a.x2 + offset; i++) {
+                for (int j = a.y1 + offset; j < a.y2 + offset; j++) {
                     board[i][j] = 1;
                 }
             }
         }
-
         int cnt = 0;
         for (int i = 0; i < 200; i++) {
             for (int j = 0; j < 200; j++) {
