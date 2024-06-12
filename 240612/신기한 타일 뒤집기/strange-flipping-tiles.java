49c3e595d4b91faa4554eab9a8c2e782a32ba169
SeongJun Ahn <dksckd0804@naver.com>
2024-06-12 14:57:58 +0900

    Title: 신기한 타일 뒤집기, Time: 198ms, Memory: 13MB, Status: Wrong Answer - CodeTree

---
+++ 240612/신기한 타일 뒤집기/strange-flipping-tiles.java
@@ -0,0 +1,94 @@
+import java.util.*;
+
+/*
+1. offset 해야하므로 배열의 전체 크기 구하기
+2. 색깔 변화 체크할 배열 생성하기
+
+3. for문 돌면서 시뮬레이션
+*/
+
+class Order {
+    int value;
+    String order;
+
+    public Order(int value, String order) {
+        this.value = value;
+        this.order = order;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        Order[] orders = new Order[n];
+
+        //전체배열 크기를 구하기 위한 임시 배열
+        List<Integer> tmp = new ArrayList<Integer>();
+
+        int curr = 0;
+        for (int i = 0; i < n; i++) {
+            int value = sc.nextInt();
+            String order = sc.next();
+
+            orders[i] = new Order(value, order);
+
+            if (order.equals("R")) {
+                tmp.add(curr + value);
+                curr += value;
+            } else {
+                tmp.add(curr - value);
+                curr -= value;
+            }
+        }
+
+        // offset 값 구하기 위해 배열크기 및 최대최소 구하기
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (Integer t : tmp) {
+            if (t < min) {
+                min = t;
+            }
+
+            if (t > max) {
+                max = t;
+            }
+        }
+
+        int arrSize = max - min;
+        int offset = min * -1;
+
+        String[] colorArr = new String[arrSize];
+
+        int idx = offset;
+        for (Order o : orders) {
+            if (o.order.equals("R")) {
+                for (int i = idx; i < idx + o.value; i++) {
+                    colorArr[i] = "B";
+                }
+                idx += o.value;
+            } else {
+                for (int i = idx-1; i >= idx - o.value; i--) {
+                    colorArr[i] = "W";
+                }
+                idx -= o.value;
+            }
+        }
+
+        int white = 0;
+        int black = 0;
+
+        for (int i = 0; i < arrSize; i++) {
+            if (colorArr[i].equals("B")) {
+                black++;
+            } else {
+                white++;
+            }
+        }
+
+        System.out.println(white + " " + black);
+    }
+}
