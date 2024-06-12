7cfdd99dbc42bf9dcd8672a76e6cd0cf0b13ef7d
SeongJun Ahn <dksckd0804@naver.com>
2024-06-12 14:30:38 +0900

    Title: 흰검 칠하기, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240612/흰검 칠하기/painting-white-black.java
+++ 240612/흰검 칠하기/painting-white-black.java
@@ -61,32 +61,32 @@ public class Main {
         int arrSize = max - min;
         int offset = min * -1;
 
-        int[] simArr = new int[arrSize]; // 횟수 저장할곳
+        int[] blackArr = new int[arrSize]; // 횟수 저장할곳
+        int[] whiteArr = new int[arrSize];
         String[] colorArr = new String[arrSize];
 
         int idx = offset;
         for (Order o : orders) {
             if (o.order.equals("R")) {
                 for (int i = idx; i < idx + o.value; i++) {
-                    simArr[i]++;
+                    blackArr[i]++;
                     colorArr[i] = "B";
                 }
                 idx += o.value;
             } else {
                 for (int i = idx-1; i >= idx - o.value; i--) {
-                    simArr[i]++;
+                    whiteArr[i]++;
                     colorArr[i] = "W";
                 }
                 idx -= o.value;
             }
         }
-
         int white = 0;
         int black = 0;
         int gray = 0;
 
         for (int i = 0; i < arrSize; i++) {
-            if (simArr[i] >= 4) {
+            if (blackArr[i] >= 2 && whiteArr[i] >= 2) {
                 gray++;
             } else {
                 if (colorArr[i].equals("W")) {
