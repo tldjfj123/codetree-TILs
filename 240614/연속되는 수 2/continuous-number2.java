465c0318b38226d8118ecd5139c796e8f928bd3e
SeongJun Ahn <dksckd0804@naver.com>
2024-06-14 11:01:11 +0900

    Title: 연속되는 수 2, Time: 218ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240614/연속되는 수 2/continuous-number2.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int cnt = 0;
+        int max = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (i == 0 || arr[i] == arr[i-1]) {
+                cnt++;
+                max = Math.max(cnt, max);
+            } else {
+                cnt = 1;
+            }
+        }
+
+        System.out.println(max);
+
+
+    }
+}
