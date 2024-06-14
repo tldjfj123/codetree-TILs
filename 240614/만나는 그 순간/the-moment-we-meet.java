13c4f539bb99690abd6517a14fcd34ebdd1a7875
SeongJun Ahn <dksckd0804@naver.com>
2024-06-14 14:15:54 +0900

    Title: 만나는 그 순간, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240614/만나는 그 순간/the-moment-we-meet.java
@@ -0,0 +1,84 @@
+import java.util.*;
+
+class Order {
+    String order;
+    int value;
+
+    public Order(String order, int value) {
+        this.order = order;
+        this.value = value;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        Order[] aOrders = new Order[N];
+        Order[] bOrders = new Order[M];
+
+        int[] statusA = new int[1000];
+        int[] statusB = new int[1000];
+
+        for (int i = 0; i < N; i++) {
+            aOrders[i] = new Order(sc.next(), sc.nextInt());
+
+        }
+
+        for (int i = 0; i < M; i++) {
+            bOrders[i] = new Order(sc.next(), sc.nextInt());
+        }
+
+        int aIdx = 0;
+        int aPos = 0;
+        for (Order a : aOrders) {
+            if (a.order.equals("R")) {
+                for (int i = aIdx; i < aIdx + a.value; i++) {
+                    statusA[i] = aPos;    
+                    aPos++;
+                }
+            } else {
+                for (int i = aIdx; i < aIdx + a.value; i++) {
+                    statusA[i] = aPos;    
+                    aPos--;
+                }
+            }
+            aIdx += a.value;
+                
+        }
+
+        int bIdx = 0;
+        int bPos = 0;
+        for (Order b : bOrders) {
+            if (b.order.equals("R")) {
+                for (int i = bIdx; i < bIdx + b.value; i++) {
+                    statusB[i] = bPos;    
+                    bPos++;
+                }
+            } else {
+                for (int i = bIdx; i < bIdx + b.value; i++) {
+                    statusB[i] = bPos;    
+                    bPos--;
+                }
+            }
+            bIdx += b.value;   
+        }
+        
+        int res = -1;
+        for (int i = 1; i < 1000; i++) {
+            if (statusA[i] == statusB[i]) {
+                res = i;
+                break;
+            }
+        }
+
+        System.out.println(res);
+
+
+
+
+    }
+}
