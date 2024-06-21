4c279032aaa2ed49a7cf5101ab31a1f0dfe2940a
SeongJun Ahn <dksckd0804@naver.com>
2024-06-21 14:39:45 +0900

    Title: 개발팀의 능력, Time: 140ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240621/개발팀의 능력/the-capabilities-of-the-development-team.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[5];
+
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            arr[i] = sc.nextInt();
+            sum += arr[i];
+        }
+
+        int res = Integer.MAX_VALUE;
+        for (int i = 0; i < 5; i++) {
+            int team1 = arr[i]; // 깍두기 1명 정하기
+
+            for (int j = 0; j < 5; j++) {
+                for (int k = j+1; k < 5; k++) { //2명 정하기 (깍두기 제거 유의)
+                    if (j != i && k != i) {
+                        int t1 = arr[j];
+                        int t2 = arr[k];
+                        
+                        int team2 = t1 + t2;
+                        int team3 = sum - team1 - team2;
+
+                        if (team1 != team2 && team2 != team3) {
+                            int max = Math.max(team1, Math.max(team2, team3));
+                            int min = Math.min(team1, Math.min(team2, team3));
+
+                            res = Math.min(res, Math.abs(max - min));
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.println(res);
+    }
+}
