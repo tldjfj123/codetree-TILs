01362c27a321fae0f0e86184cda0f1edeff86b83
SeongJun Ahn <dksckd0804@naver.com>
2024-06-24 14:43:52 +0900

    Title: 겹치지 않는 선분 2, Time: 161ms, Memory: 10MB, Status: Passed - CodeTree

--- 240624/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.java
+++ 240624/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.java
@@ -1,12 +1,22 @@
 import java.util.*;
 
-class Point {
+class Point implements Comparable<Point>{
     int x1;
     int x2;
 
     public Point(int x1, int x2) {
         this.x1 = x1;
         this.x2 = x2;
+    }
+
+    @Override
+    public int compareTo(Point p) {
+        return this.x1 - p.x1;
+    }
+
+    @Override
+    public String toString() {
+        return x1 + " " + x2;
     }
 }
 
@@ -19,37 +29,57 @@ public class Main {
         Point[] arr = new Point[N];
 
         for (int i = 0; i < N; i++) {
-            int x1 = sc.nextInt();
-            int x2 = sc.nextInt();
-
-            arr[i] = new Point(x1, x2);
+            arr[i] = new Point(sc.nextInt(), sc.nextInt());
         }
 
-        int cnt = 0;
-        boolean[] check = new boolean[N];
-        for (int i = 0; i < arr.length; i++) {
-            int start = arr[i].x1;
-            int end = arr[i].x2;
-            // System.out.println("Start : " + start + " End : " + end);
+        //시작점 내림차순 정렬
+        Arrays.sort(arr);
 
+        // System.out.println(Arrays.toString(arr));
+        int res = N;
+
+        for (int i = 0; i < arr.length; i++) {
+            boolean isBack = false;
+            int iStart = arr[i].x1;
+            int iEnd = arr[i].x2;
+
+            if (iStart > iEnd) {
+                isBack = true;
+            }
+            
             for (int j = i+1; j < arr.length; j++) {
-                if ((start <= arr[j].x1 && arr[j].x2 <= end) || (arr[j].x1 <= start && end <= arr[j].x2)) {
-                    // System.out.println("arr[j].x1 : " + arr[j].x1 + " arr[j].x2 : " + arr[j].x2);
-                    check[i] = true;
-                    check[j] = true;
-                    break;
+                // System.out.println(arr[i] + " " + arr[j]);
+                int jStart = arr[j].x1;
+                int jEnd = arr[j].x2;
+                if (isBack) { 
+                    if (jStart > jEnd) { // 둘다 왼쪽방향
+                        if ((iEnd <= jEnd && jStart <= iStart) || (jEnd <= iEnd && iStart <= jStart)) {
+                            res -= 2;
+                        }
+                    } else { // 한놈은 왼쪽 한놈은 오른쪽
+                        if (iEnd <= jStart && jStart <= iStart) {
+                            res -= 2;
+                        }   
+                    }
+                } else {
+                    if (jStart > jEnd) { // 한놈은 오른쪽 한놈은 왼쪽
+                        if (iStart <= jEnd && jEnd <= iEnd) {
+                            res -= 2;
+                        }
+                    } else { // 둘다 오른쪽
+                        if ((iStart <= jStart && jEnd <= iEnd) || (jStart <= iStart && iEnd <= jEnd)) {
+                            res -= 2;
+                        }
+                    }
+
                 }
             }
-         }
-        
-        int res = 0;
-
-        for (boolean c : check) {
-            if(!c) {
-                res++;
-            }
         }
-
-        System.out.println(res);
+        
+        if (res > 0) {
+            System.out.println(res);
+        } else {
+            System.out.println(0);
+        }
     }
 }
