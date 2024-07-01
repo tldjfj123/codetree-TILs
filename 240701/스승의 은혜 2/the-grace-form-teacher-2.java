1e32e3dd9b1f34da548767d94631c0d214612e69
SeongJun Ahn <dksckd0804@naver.com>
2024-07-01 13:02:18 +0900

    Title: 스승의 은혜 2, Time: 222ms, Memory: 13MB, Status: Passed - CodeTree

--- 240701/스승의 은혜 2/the-grace-form-teacher-2.java
+++ 240701/스승의 은혜 2/the-grace-form-teacher-2.java
@@ -12,14 +12,16 @@ public class Main {
         for (int i = 0; i < N; i++) {
             arr[i] = sc.nextInt();
         }
+        
+        Arrays.sort(arr);
 
         int res = 0;
-        for (int i = 0; i < N; i++) {
-            int cnt = 0;
-            int budget = B;
-            int calc = 0;
+        for (int i = 0; i < N; i++) { // 할인할 학생 고르기
+            int cnt = 0; // 사람 수
+            int budget = B; // 예산
+            int calc = 0; // 물품 값
 
-            for (int j = 0; j < N; j++) {
+            for (int j = 0; j < N; j++) { // 결제 
                 if (i == j) {
                     calc = arr[j] / 2;        
                 } else {
