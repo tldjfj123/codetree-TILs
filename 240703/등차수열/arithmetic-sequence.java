825ebccab2237f7ab1dbd9c2e0f56276ab2181a1
SeongJun Ahn <dksckd0804@naver.com>
2024-07-03 13:23:39 +0900

    Title: 등차수열, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240703/등차수열/arithmetic-sequence.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int v = sc.nextInt();
+            arr[i] = v;
+        }
+
+        if (n < 2) {
+            System.out.println(0);
+            return;
+        }
+
+        Arrays.sort(arr);
+
+        // System.out.println(Arrays.toString(arr));
+
+        /*
+        1. 숫자가 앞에 붙는 경우
+        2. 숫자가 가운데 붙는 경우
+        3. 숫자가 끝에 붙는 경우
+        */
+
+        int cnt = 0;
+        for (int t = 1; t <= 100; t++) {
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = i+1; j < arr.length; j++) {
+                    if (Math.abs(t - arr[i]) == Math.abs(t - arr[j])) {
+                        cnt++;
+                    }
+                }
+            }            
+        }
+
+        System.out.println(cnt);
+    }
+}
