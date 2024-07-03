db2ca300ecff194f90a8e1750290e42203fa0fea
SeongJun Ahn <dksckd0804@naver.com>
2024-07-03 13:26:19 +0900

    Title: 등차수열, Time: 164ms, Memory: 10MB, Status: Passed - CodeTree

--- 240703/등차수열/arithmetic-sequence.java
+++ 240703/등차수열/arithmetic-sequence.java
@@ -28,17 +28,19 @@ public class Main {
         3. 숫자가 끝에 붙는 경우
         */
 
-        int cnt = 0;
+        int res = 0;
         for (int t = 1; t <= 100; t++) {
+            int cnt = 0;
             for (int i = 0; i < arr.length; i++) {
                 for (int j = i+1; j < arr.length; j++) {
                     if (Math.abs(t - arr[i]) == Math.abs(t - arr[j])) {
                         cnt++;
                     }
                 }
-            }            
+            }  
+            res = Math.max(res, cnt);
         }
 
-        System.out.println(cnt);
+        System.out.println(res);
     }
 }
