b4857e7d3a1ab7cbd6457d370e3301609831c402
SeongJun Ahn <dksckd0804@naver.com>
2024-07-03 11:30:16 +0900

    Title: 흥미로운 숫자 2, Time: 166ms, Memory: 12MB, Status: Wrong Answer - CodeTree

---
+++ 240703/흥미로운 숫자 2/interesting-numbers-2.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Main {
+    public static int calc(int x) {
+        String s = String.valueOf(x);
+        char[] arr = s.toCharArray();
+
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (!map.containsKey(arr[i])) {
+                map.put(arr[i], 1);
+            } else {
+                int v = map.get(arr[i]);
+                map.put(arr[i], v+1);
+            }
+        }
+        
+
+        if (map.keySet().size() == 2) {
+            return 1;
+        }
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int X = sc.nextInt();
+        int Y = sc.nextInt();
+
+        int res = 0;
+        for (int i = X; i <= Y; i++) {
+            res += calc(i);
+        }
+
+        System.out.println(res);
+    }
+}
