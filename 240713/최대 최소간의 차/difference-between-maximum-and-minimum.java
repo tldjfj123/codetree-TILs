f122722eaf8aedeba9e138427d5b39bf111f199c
SeongJun Ahn <dksckd0804@naver.com>
2024-07-13 13:57:14 +0900

    Title: 최대 최소간의 차, Time: 215ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240713/최대 최소간의 차/difference-between-maximum-and-minimum.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        int res = Integer.MAX_VALUE;
+        for (int i = 1; i <= 10000 - k; i++) {
+            int min = i;
+            int max = i + k;
+
+            int tmp = 0;
+            for (int j = 0; j < n; j++) {
+                if (arr[j] < min || arr[j] > max) {
+                    tmp += Math.min(Math.abs(arr[j] - min), Math.abs(arr[j] - max));
+                }
+                
+            }
+
+            res = Math.min(tmp, res);
+        }
+
+        System.out.println(res);
+    }
+}
