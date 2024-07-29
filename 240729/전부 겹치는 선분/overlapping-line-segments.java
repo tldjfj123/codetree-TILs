d556e4319d21c2066cc6bd6beb5b7d6526b82805
SeongJun Ahn <dksckd0804@naver.com>
2024-07-29 12:07:23 +0900

    Title: 전부 겹치는 선분, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

--- 240729/전부 겹치는 선분/overlapping-line-segments.java
+++ 240729/전부 겹치는 선분/overlapping-line-segments.java
@@ -1,71 +1,32 @@
 import java.util.*;
 
-class Line implements Comparable<Line> {
-    int x1;
-    int x2;
-
-    public Line(int x1, int x2) {
-        this.x1 = x1;
-        this.x2 = x2;
-    }
-
-    @Override
-    public String toString() {
-        return this.x1 + " " + this.x2; 
-    }
-
-    @Override
-    public int compareTo(Line l) {
-        return this.x1 - l.x1;
-    }
-
-}
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        Line[] arr = new Line[n];
+        int[] arr = new int[101];
 
         for (int i = 0; i < n; i++) {
-            int s1 = sc.nextInt();
-            int s2 = sc.nextInt();
+            int x1 = sc.nextInt();
+            int x2 = sc.nextInt();
 
-            arr[i] = new Line(s1, s2);
-        }
-
-        Arrays.sort(arr);
-        // System.out.println(Arrays.toString(arr));
-
-        int start = 0;
-        int end = 0;
-
-        for (int i = 0; i < arr.length-1; i++) {
-            Line left = arr[i];
-            Line right = arr[i+1];
-
-            // System.out.println(start + " " + end);
-
-            if (left.x1 <= right.x1 && right.x2 <= left.x2) {
-                start = right.x1;
-                end = right.x2;
-            } else if (left.x2 <= right.x1) {
-                start = left.x2;
-                end = right.x1;
+            for (int j = x1; j <= x2; j++) {
+                arr[j]++;
             }
         }
 
-        // System.out.println(start + " " + end);
+        boolean flag = false;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == n) {
+                flag = true;
+            }
+        }
 
-        if (start != 0 || end != 0) {
+        if (flag) {
             System.out.println("Yes");
         } else {
             System.out.println("No");
         }
-
-
-
-
     }
 }
