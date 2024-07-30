e25da1eb4142a50f89123d419f6e49e06ac0dae8
SeongJun Ahn <dksckd0804@naver.com>
2024-07-30 16:06:45 +0900

    Title: 달리자, Time: 138ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240730/달리자/run-and-run.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arrA = new int[n];
+        int[] arrB = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arrA[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            arrB[i] = sc.nextInt();
+        }
+
+        int cnt = 0;
+        for (int i = 0; i < n-1; i++) {
+            int gap = arrB[i] - arrA[i];
+            if (gap > 0) {
+                cnt += gap;
+                arrA[i] += gap;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
