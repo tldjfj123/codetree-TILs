8f270fe4a56addaec96290edf7eeab775d03cdff
SeongJun Ahn <dksckd0804@naver.com>
2024-08-12 16:12:50 +0900

    Title: 연속된 숫자 만들기 2, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

--- 240812/연속된 숫자 만들기 2/create-consecutive-numbers-2.java
+++ 240812/연속된 숫자 만들기 2/create-consecutive-numbers-2.java
@@ -1,36 +1,52 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
 
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
-
-        int cnt = 0;
+        
+        int count = 0;
 
         while (true) {
-            // 위치를 정렬하지 않고, 각 위치 간의 간격을 계산합니다.
-            int gap1 = Math.abs(b - a);
-            int gap2 = Math.abs(c - b);
+            int gap1 = Math.abs(a - b);
+            int gap2 = Math.abs(b - c);
 
-            // 이미 연속된 경우 종료
+            // 거리가 모두 1이면 이미 연속한 수
             if (gap1 == 1 && gap2 == 1) {
-                System.out.println(cnt);
                 break;
             }
 
-            // 가장 큰 간격을 좁히는 방법을 선택합니다.
-            if (gap1 > gap2) {
-                // a와 b 사이의 간격이 더 크다면 a를 이동
-                a = b + 1;
-            } else {
-                // b와 c 사이의 간격이 더 크다면 c를 이동
-                c = b - 1;
+            // 하나라도 거리가 2이면 한 번의 연산만 더 수행해주면 완성
+            if (gap1 == 2 || gap2 == 2) {
+                count++;
+                break;
             }
 
-            cnt++;
+            // 각 둘씩 잡았을 때의 거리가 모두 2보다 크면
+            // 거리가 가까운 쪽 안에 반대편 수를 넣어준다.
+            if (gap1 > 2 && gap2 > 2) {
+                if (gap1 < gap2) {
+                    c = b;
+                    b = a + 2;
+                } else {
+                    a = b;
+                    b = c + 2;
+                }
+            } else if (b > 2) {
+                a = b;
+                b = b + 2;
+            } else if (a > 2) {
+                c = b;
+                c = a + 2;
+            }
+
+            count++;
         }
+
+        System.out.println(count);
     }
 }
