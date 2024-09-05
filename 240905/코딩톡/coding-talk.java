71c47927d7e3754bf658056477798c2dfe2e3825
SeongJun Ahn <dksckd0804@naver.com>
2024-09-05 16:27:07 +0900

    Title: 코딩톡, Time: 163ms, Memory: 10MB, Status: Passed - CodeTree

--- 240905/코딩톡/coding-talk.java
+++ 240905/코딩톡/coding-talk.java
@@ -1,51 +1,41 @@
-import java.util.*;
-class Message {
-    char sender;
-    int unread;
-
-    public Message(char sender, int unread) {
-        this.sender = sender;
-        this.unread = unread;
-    }
-}
+import java.util.Scanner;
 
 public class Main {
+    public static final int MAX_M = 100;
+    
+    public static int n, p, m;
+    public static char[] c = new char[MAX_M];
+    public static int[] u = new int[MAX_M];
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int p = sc.nextInt();
-
-        int[] people = new int[n];
-        Message[] arr = new Message[m];
-
-        for (int i = 0; i < m; i++) {
-            char sender = sc.next().charAt(0);
-            int unread = sc.nextInt();
-            arr[i] = new Message(sender, unread);
+        // 입력
+        n = sc.nextInt();
+        m = sc.nextInt();
+        p = sc.nextInt();
+        for(int i = 0; i < m; i++) {
+            c[i] = sc.next().charAt(0);
+            u[i] = sc.nextInt();
         }
 
-        for (int i = p-1; i < m; i++) {
-            int read = (int)arr[i].sender - 65;
-            people[read] = 1;
-            
+        // 모두 읽은 채팅이라면 읽지 않은 사람은 없습니다.
+        if(u[p - 1] == 0)
+            System.exit(0);
+        
+        // 각 사람에 대해 채팅을 읽었을지 안 읽었을지 판단합니다.
+        for(int i = 0; i < n; i++) {
+            // read : 확실하게 채팅을 읽었으면 true
+            char person = (char)((int)'A' + i);
+            boolean read = false;
+
+            // 만약 p번 메시지를 읽은 사람 수와 같은 채팅을 기준으로
+            // 한번이라도 채팅을 쳤다면 확실하게 채팅을 읽었습니다.
+            for(int j = 0; j < m; j++)
+                if(u[j] >= u[p - 1] && c[j] == person)
+                    read = true;
+                
+            if(read == false)
+                System.out.print(person + " ");
         }
-
-        // for (int i = 0; i < p; i++) {
-        //     if(arr[i].unread == 0) {
-        //         for (int j = 0; j < n; j++) {
-        //             people[j] = 1;
-        //         }
-        //     }
-        // }
-
-        for (int i = 0; i < n; i++) {
-            if (people[i] == 0) {
-                System.out.print((char)(65 + i) + " ");
-            }
-        }
-
-
     }
 }
