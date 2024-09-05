6eba73ed01c10d34f0b4bf0023c0e3367b4bdcd0
SeongJun Ahn <dksckd0804@naver.com>
2024-09-05 16:04:29 +0900

    Title: 코딩톡, Time: 110ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240905/코딩톡/coding-talk.java
@@ -0,0 +1,43 @@
+import java.util.*;
+class Message {
+    char sender;
+    int unread;
+
+    public Message(char sender, int unread) {
+        this.sender = sender;
+        this.unread = unread;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int p = sc.nextInt();
+
+        int[] people = new int[n];
+        Message[] arr = new Message[m];
+
+        for (int i = 0; i < m; i++) {
+            char sender = sc.next().charAt(0);
+            int unread = sc.nextInt();
+            arr[i] = new Message(sender, unread);
+        }
+
+        for (int i = p-1; i < m; i++) {
+            int read = (int)arr[i].sender - 65;
+            people[read] = 1;
+            
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (people[i] == 0) {
+                System.out.print((char)(65 + i) + " ");
+            }
+        }
+
+
+    }
+}
