c43a47711d24cb2ce4e62595da6873dbcf24ccb7
SeongJun Ahn <dksckd0804@naver.com>
2024-09-06 13:44:41 +0900

    Title: 독서실의 거리두기 3, Time: 140ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240906/독서실의 거리두기 3/study-cafe-keeping-distance-3.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        char[] arr = sc.next().toCharArray();
+        
+        List<Integer> indexes = new ArrayList<Integer>();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == '1') {
+                indexes.add(i);
+            }
+        }
+
+        int[] res = new int[indexes.size()];
+        int idx = 0;
+        for (int index : indexes) {
+            res[idx] = index;
+            idx++;
+        }
+
+        // System.out.println(Arrays.toString(res));
+
+        int fin = 0;
+        for (int i = 0; i < res.length-1; i++) {
+            fin = Math.max(fin, (res[i+1] - res[i]) / 2);
+            // System.out.println(fin);
+        }
+
+        System.out.println(fin);
+
+        
+    }
+}
