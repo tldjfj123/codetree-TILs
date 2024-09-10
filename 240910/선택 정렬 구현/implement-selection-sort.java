07539726fec953fb9ac6e52906161e13652bae98
SeongJun Ahn <dksckd0804@naver.com>
2024-09-10 12:55:06 +0900

    Title: 선택 정렬 구현, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

--- 240910/선택 정렬 구현/implement-selection-sort.java
+++ 240910/선택 정렬 구현/implement-selection-sort.java
@@ -2,15 +2,14 @@ import java.util.*;
 
 public class Main {
     static void swap(int[] arr, int i, int j) {
-        if (arr[i] > arr[j]) {
-            int tmp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = tmp;
-        }
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         int n = sc.nextInt();
         int[] arr = new int[n];
 
@@ -18,19 +17,26 @@ public class Main {
             arr[i] = sc.nextInt();
         }
 
-        for (int i = 0; i < n; i++) {
+        // 선택 정렬 알고리즘
+        for (int i = 0; i < n - 1; i++) {
             int min = i;
 
-            for (int j = i+1; j < n; j++) {
-                if (arr[min] > arr[j]) {
-                    swap(arr, i, j);
+            // 최소값의 인덱스를 찾는 내부 루프
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
                 }
+            }
+
+            // 최소값을 현재 위치와 교환
+            if (min != i) {
+                swap(arr, i, min);
             }
         }
 
+        // 정렬된 배열 출력
         for (int a : arr) {
             System.out.print(a + " ");
         }
-
     }
 }
