679338f949fc0f009c14c2aab1d5ccc86d174e87
SeongJun Ahn <dksckd0804@naver.com>
2024-09-10 15:31:17 +0900

    Title: 삽입 정렬 구현, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240910/삽입 정렬 구현/implement-insertion-sort.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+
+        // 결과 출력
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
